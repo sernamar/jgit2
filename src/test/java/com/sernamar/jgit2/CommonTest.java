aa4047ce77140216f83e2977839bac2ec7515029
sernamar <sernamar@protonmail.com>
2025-03-24 21:00:45 +0100

    Add setup and teardown methods for CommonTest
    Added @BeforeAll and @AfterAll methods to initialize and shutdown libgit2 in CommonTest. This ensures proper setup and teardown for all tests in the class.

--- src/test/java/com/sernamar/jgit2/CommonTest.java
+++ src/test/java/com/sernamar/jgit2/CommonTest.java
@@ -1,10 +1,23 @@
 package com.sernamar.jgit2;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.sernamar.jgit2.Global.gitLibgit2Init;
+import static com.sernamar.jgit2.Global.gitLibgit2Shutdown;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonTest {
+    @BeforeAll
+    static void beforeAll() {
+        gitLibgit2Init();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        gitLibgit2Shutdown();
+    }
 
     @Test
     void gitLibgit2Version() {
