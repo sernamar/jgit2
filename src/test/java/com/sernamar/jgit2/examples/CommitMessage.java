8bf0388cacd7fb3a98f9e94a407bccbe35688830
sernamar <sernamar@protonmail.com>
2025-03-24 05:08:01 +0100

    Add example for retrieving last commit message

--- src/test/java/com/sernamar/jgit2/examples/CommitMessage.java
+++ src/test/java/com/sernamar/jgit2/examples/CommitMessage.java
@@ -1,20 +1,27 @@
-package com.sernamar.jgit2;
+package com.sernamar.jgit2.examples;
+
+import com.sernamar.jgit2.*;
 
 import java.nio.file.Paths;
 
-public class Scratch {
+public class CommitMessage {
     public static void main(String[] args) {
         // Initialize libgit2
         Global.gitLibgit2Init();
 
-        String path = Paths.get("").toAbsolutePath().toString();
+        // Get the current path
+        var path = Paths.get("").toAbsolutePath().toString();
+
         // Open repository
-        GitRepository repo = Repository.gitRepositoryOpen(path);
+        var repo = Repository.gitRepositoryOpen(path);
+
         // Get the reference id of the main branch (refs/heads/main)
-        String reference = "refs/heads/main";
-        GitOid referenceId = Refs.gitReferenceNameToId(repo, reference);
+        var reference = "refs/heads/main";
+        var referenceId = Refs.gitReferenceNameToId(repo, reference);
+
         // Get last commit
-        GitCommit commit = Commit.gitCommitLookup(repo, referenceId);
+        var commit = Commit.gitCommitLookup(repo, referenceId);
+
         // Get commit message and print it
         String message = Commit.gitCommitMessage(commit);
         System.out.println("Commit message:");
