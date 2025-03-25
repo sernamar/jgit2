19ec7975e2e1c5d50311fbfa41b86cb331a54345
sernamar <sernamar@protonmail.com>
2025-03-25 10:42:49 +0100

    Remove AfterAll methods due to issues during test execution
    Remove AfterAll methods for gitLibgit2Shutdown due to Maven and JUnit
    Jupiter test execution issues. Not sure why though. It may be due to
    concurrency issues, or improper resource cleanup timing, or somthing else.

--- src/test/java/com/sernamar/jgit2/CommonTest.java
+++ src/test/java/com/sernamar/jgit2/CommonTest.java
@@ -1,22 +1,16 @@
 package com.sernamar.jgit2;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.sernamar.jgit2.Global.gitLibgit2Init;
-import static com.sernamar.jgit2.Global.gitLibgit2Shutdown;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonTest {
+
     @BeforeAll
     static void beforeAll() {
         gitLibgit2Init();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        gitLibgit2Shutdown();
     }
 
     @Test
--- src/test/java/com/sernamar/jgit2/OidTest.java
+++ src/test/java/com/sernamar/jgit2/OidTest.java
@@ -1,12 +1,10 @@
 package com.sernamar.jgit2;
 
 import com.sernamar.jgit2.types.GitOidShorten;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.sernamar.jgit2.Global.gitLibgit2Init;
-import static com.sernamar.jgit2.Global.gitLibgit2Shutdown;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OidTest {
@@ -14,11 +12,6 @@ class OidTest {
     @BeforeAll
     static void beforeAll() {
         gitLibgit2Init();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        gitLibgit2Shutdown();
     }
 
     @Test
