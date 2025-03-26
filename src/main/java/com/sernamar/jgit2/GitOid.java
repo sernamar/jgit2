bcdcdfeb28c98520d347b6b115790640f2a0fbd7
sernamar <sernamar@protonmail.com>
2025-03-26 06:00:42 +0100

    Improve GitOid note about memory management

--- src/main/java/com/sernamar/jgit2/GitOid.java
+++ src/main/java/com/sernamar/jgit2/GitOid.java
@@ -2,8 +2,8 @@ package com.sernamar.jgit2;
 
 import java.lang.foreign.MemorySegment;
 
-// NOTE: this class does not implement the AutoCloseable interface,
-// as there is not a git_oid_free() function in `libgit2`
+// NOTE: This class does not implement `AutoCloseable` because `libgit2` does not provide a `git_oid_free()` function.
+// `GitOid` represents an immutable value in the underlying C library, so it does not require manual memory management.
 public final class GitOid {
     private final MemorySegment segment;
 
