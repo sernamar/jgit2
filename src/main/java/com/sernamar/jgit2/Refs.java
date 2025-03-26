ddde58fd45b24d0083444d2f9055f19b7cc8f0f8
sernamar <sernamar@protonmail.com>
2025-03-26 19:04:47 +0100

    Fix issues with error handling in gitReferenceNameToId method
    + Add specific exceptions for invalid reference names and not found
    references.
    + Update method documentation to reflect new behavior.

--- src/main/java/com/sernamar/jgit2/Refs.java
+++ src/main/java/com/sernamar/jgit2/Refs.java
@@ -5,6 +5,8 @@ import com.sernamar.jgit2.bindings.git_oid;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
+import static com.sernamar.jgit2.bindings.git2_1.GIT_EINVALIDSPEC;
+import static com.sernamar.jgit2.bindings.git2_1.GIT_ENOTFOUND;
 import static com.sernamar.jgit2.bindings.git2_2.git_reference_name_to_id;
 import static com.sernamar.jgit2.utils.GitError.getGitErrorMessage;
 
@@ -26,7 +28,9 @@ public final class Refs {
      *
      * @param repo The repository in which to look up the reference
      * @param name The long name for the reference (e.g. HEAD, refs/heads/master, refs/tags/v0.1.0, ...)
-     * @return the OID of the reference.
+     * @return the OID of the reference, or `null` if the reference is not found,
+     *
+     * @throws RuntimeException if the reference name is invalid or if an error occurs.
      */
     public static GitOid gitReferenceNameToId(GitRepository repo, String name) {
         Arena arena = Arena.ofAuto();
@@ -34,9 +38,15 @@ public final class Refs {
         MemorySegment refNameSegment = arena.allocateFrom(name);
 
         int ret = git_reference_name_to_id(idSegment, repo.segment(), refNameSegment);
-        if (ret < 0) {
+
+        if (ret == GIT_ENOTFOUND()) {
+            return null;
+        } else if (ret == GIT_EINVALIDSPEC()) {
+            throw new RuntimeException("Invalid reference name: " + name);
+        } else if (ret < 0) {
             throw new RuntimeException("Failed to get the OID of the reference: " + getGitErrorMessage());
         }
+
         return new GitOid(idSegment);
     }
 }
