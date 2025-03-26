13d3a30f2e516c5ec75de6d7117acfba81ef5b75
sernamar <sernamar@protonmail.com>
2025-03-26 20:59:08 +0100

    Add missing Javadocs in gitCommitFree and gitCommitCreateV methods

--- src/main/java/com/sernamar/jgit2/Commit.java
+++ src/main/java/com/sernamar/jgit2/Commit.java
@@ -46,10 +46,56 @@ public final class Commit {
         return git_commit_message(commit.segment()).getString(0);
     }
 
+    /**
+     * Close an open commit
+     * <p>
+     * This is a wrapper around git_object_free()
+     * <p>
+     * IMPORTANT:
+     * It *is* necessary to call this method when you stop
+     * using a commit. Failure to do so will cause a memory leak.
+     *
+     * @param commit the commit to close
+     */
     public static void gitCommitFree(GitCommit commit) {
         git_commit_free(commit.segment());
     }
 
+    /**
+     * Create new commit in the repository using a variable argument list.
+     * <p>
+     * The message will **not** be cleaned up automatically. You can do that
+     * with the `git_message_prettify()` function.
+     * <p>
+     * The parents for the commit are specified as a variable list of pointers
+     * to `const git_commit *`. Note that this is a convenience method which may
+     * not be safe to export for certain languages or compilers
+     * <p>
+     * All other parameters remain the same as `git_commit_create()`.
+     *
+     * @param repo            Repository where to store the commit
+     * @param updateRef       If not NULL, name of the reference that
+     *                        will be updated to point to this commit. If the reference
+     *                        is not direct, it will be resolved to a direct reference.
+     *                        Use "HEAD" to update the HEAD of the current branch and
+     *                        make it point to this commit. If the reference doesn't
+     *                        exist yet, it will be created. If it does exist, the first
+     *                        parent must be the tip of this branch.
+     * @param author          Signature with author and author time of commit
+     * @param committer       Signature with committer and * commit time of commit
+     * @param messageEncoding The encoding for the message in the
+     *                        commit, represented with a standard encoding name.
+     *                        E.g. "UTF-8". If NULL, no encoding header is written and
+     *                        UTF-8 is assumed.
+     * @param message         Full message for this commit
+     * @param tree            An instance of a `git_tree` object that will
+     *                        be used as the tree for the commit. This tree object must
+     *                        also be owned by the given `repo`.
+     * @param parents         Variable argument list of parents for this commit.
+     * @return the OID of the newly created commit.
+     * The created commit will be written to the Object Database and
+     * the given reference will be updated to point to it
+     */
     public static GitOid gitCommitCreateV(GitRepository repo,
                                           String updateRef,
                                           GitSignature author,
