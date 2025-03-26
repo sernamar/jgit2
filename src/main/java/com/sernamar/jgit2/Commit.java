75b20587eaeba8b0230eed126c09cf8ed8dc2e93
sernamar <sernamar@protonmail.com>
2025-03-26 20:51:52 +0100

    Add gitCommitCreateV method for variadic commit creation

--- src/main/java/com/sernamar/jgit2/Commit.java
+++ src/main/java/com/sernamar/jgit2/Commit.java
@@ -1,7 +1,11 @@
 package com.sernamar.jgit2;
 
+import com.sernamar.jgit2.bindings.git_oid;
+
 import java.lang.foreign.Arena;
+import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
+import java.util.Arrays;
 
 import static com.sernamar.jgit2.bindings.git2_2.*;
 import static com.sernamar.jgit2.utils.GitError.getGitErrorMessage;
@@ -44,5 +48,50 @@ public final class Commit {
 
     public static void gitCommitFree(GitCommit commit) {
         git_commit_free(commit.segment());
+    }
+
+    public static GitOid gitCommitCreateV(GitRepository repo,
+                                          String updateRef,
+                                          GitSignature author,
+                                          GitSignature committer,
+                                          String messageEncoding,
+                                          String message,
+                                          GitTree tree,
+                                          GitCommit... parents) {
+        Arena arena = Arena.ofAuto();
+        MemorySegment idSegment = git_oid.allocate(arena);
+        MemorySegment updateRefSegment = arena.allocateFrom(updateRef);
+        MemorySegment messageEncodingSegment = arena.allocateFrom(messageEncoding);
+        MemorySegment messageSegment = arena.allocateFrom(message);
+
+        // Convert parents to an array of MemorySegments
+        MemorySegment[] parentSegments = Arrays.stream(parents)
+                .map(GitCommit::segment)
+                .toArray(MemorySegment[]::new);
+
+        // Create a FunctionDescriptor for the variadic parameters
+        MemoryLayout[] variadicLayouts = new MemoryLayout[parents.length];
+        Arrays.fill(variadicLayouts, C_POINTER);
+
+
+        // As `git_commit_create_v` is a variadic function, we need to use the `makeInvoker` method
+        // to create an invoker that can be used to call the function.
+        git_commit_create_v invoker = git_commit_create_v.makeInvoker(variadicLayouts);
+        int ret = invoker.apply(
+                idSegment,
+                repo.segment(),
+                updateRefSegment,
+                author.segment(),
+                committer.segment(),
+                messageEncodingSegment,
+                messageSegment,
+                tree.segment(),
+                parents.length,
+                (Object[]) parentSegments
+        );
+        if (ret < 0) {
+            throw new RuntimeException("Failed to create the commit: " + getGitErrorMessage());
+        }
+        return new GitOid(idSegment);
     }
 }
