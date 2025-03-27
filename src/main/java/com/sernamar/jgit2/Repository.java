f5eaa34cd8a173cc5b13c9363625c4f88b79c316
sernamar <sernamar@protonmail.com>
2025-03-27 08:49:15 +0100

    Set overloaded method for gitRepositoryInit to bare false by default

--- src/main/java/com/sernamar/jgit2/Repository.java
+++ src/main/java/com/sernamar/jgit2/Repository.java
@@ -13,7 +13,7 @@ public final class Repository {
     }
 
     /**
-     * Creates a new bare Git repository in the given folder.
+     * Creates a new Git repository in the given folder.
      * <p>
      * Note that the libgit2 library _must_ be initialized using
      * `git_libgit2_init` before any APIs can be called, including
@@ -23,15 +23,7 @@ public final class Repository {
      * @return the repository.
      */
     public static GitRepository gitRepositoryInit(String path) {
-        Arena arena = Arena.ofAuto();
-        MemorySegment repoSegment = arena.allocate(C_POINTER);
-        MemorySegment pathSegment = arena.allocateFrom(path);
-
-        int ret = git_repository_init(repoSegment, pathSegment, 0);
-        if (ret < 0) {
-            throw new RuntimeException("Failed to initialize repository: " + getGitErrorMessage());
-        }
-        return new GitRepository(repoSegment.get(C_POINTER, 0));
+        return gitRepositoryInit(path, false);
     }
 
     /**
