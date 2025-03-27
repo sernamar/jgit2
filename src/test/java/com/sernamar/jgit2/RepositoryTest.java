2c4d5c7305ef70291a31033d32b332f9675b164f
sernamar <sernamar@protonmail.com>
2025-03-27 19:43:21 +0100

    Add shutdown and upcase path in RepositoryTest

--- src/test/java/com/sernamar/jgit2/RepositoryTest.java
+++ src/test/java/com/sernamar/jgit2/RepositoryTest.java
@@ -1,5 +1,6 @@
 package com.sernamar.jgit2;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,16 +17,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RepositoryTest {
 
-    String path = "/tmp/repo";
+    private static final String PATH = "/tmp/repo";
 
     @BeforeAll
     static void beforeAll() {
         Global.gitLibgit2Init();
     }
 
+    @AfterAll
+    static void afterAll() {
+        Global.gitLibgit2Shutdown();
+    }
+
     @AfterEach
     void afterEach() throws IOException {
-        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+        try (Stream<Path> paths = Files.walk(Paths.get(PATH))) {
             paths.sorted(Comparator.reverseOrder())
                     .map(Path::toFile)
                     .forEach(File::delete);
@@ -36,7 +42,7 @@ class RepositoryTest {
     void gitRepositoryInit() {
         // Have permission to create a repository in the /tmp directory.
         assertDoesNotThrow(() -> {
-            try (GitRepository repo = Repository.gitRepositoryInit(path)) {
+            try (GitRepository repo = Repository.gitRepositoryInit(PATH)) {
                 assertNotNull(repo);
             }
         });
@@ -51,21 +57,21 @@ class RepositoryTest {
 
     @Test
     void gitRepositoryOpen() {
-        try (GitRepository _ = Repository.gitRepositoryInit(path);
-             GitRepository repo = Repository.gitRepositoryOpen(path)) {
+        try (GitRepository _ = Repository.gitRepositoryInit(PATH);
+             GitRepository repo = Repository.gitRepositoryOpen(PATH)) {
             assertNotNull(repo);
         }
     }
 
     @Test
     void gitRepositoryFree() {
-        GitRepository repo = Repository.gitRepositoryInit(path);
+        GitRepository repo = Repository.gitRepositoryInit(PATH);
         Repository.gitRepositoryFree(repo);
     }
 
     @Test
     void gitRepositoryIndex() {
-        try (GitRepository repo = Repository.gitRepositoryInit(path);
+        try (GitRepository repo = Repository.gitRepositoryInit(PATH);
              GitIndex index = Repository.gitRepositoryIndex(repo)) {
             assertNotNull(index);
         }
