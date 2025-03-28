fa402062f04a3f2fecd4a5411514298977442b1c
sernamar <sernamar@protonmail.com>
2025-03-28 05:55:30 +0100

    Add unit tests for Index class

---
+++ src/test/java/com/sernamar/jgit2/IndexTest.java
@@ -0,0 +1,74 @@
+package com.sernamar.jgit2;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.sernamar.jgit2.Repository.gitRepositoryIndex;
+import static com.sernamar.jgit2.Repository.gitRepositoryOpen;
+import static com.sernamar.jgit2.TestUtils.createRepoWithInitialCommit;
+import static com.sernamar.jgit2.TestUtils.deleteRepoDirectory;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class IndexTest {
+
+    private static final String PATH = "/tmp/repo";
+
+    @BeforeAll
+    static void beforeAll() {
+        Global.gitLibgit2Init();
+        createRepoWithInitialCommit(PATH);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        deleteRepoDirectory(PATH);
+        Global.gitLibgit2Shutdown();
+    }
+
+    @Test
+    void gitIndexAddByPath() {
+        try {
+            Files.write(Paths.get(PATH, "hello.txt"), "Hello, World!".getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (GitRepository repo = gitRepositoryOpen(PATH);
+             GitIndex index = gitRepositoryIndex(repo)) {
+            assertDoesNotThrow(() -> Index.gitIndexAddByPath(index, "hello.txt"));
+        }
+    }
+
+    @Test
+    void gitIndexWrite() {
+        try {
+            Files.write(Paths.get(PATH, "hello.txt"), "Hello, World!".getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (GitRepository repo = gitRepositoryOpen(PATH);
+             GitIndex index = gitRepositoryIndex(repo)) {
+            Index.gitIndexAddByPath(index, "hello.txt");
+            assertDoesNotThrow(() -> Index.gitIndexWrite(index));
+        }
+    }
+
+    @Test
+    void gitIndexWriteTree() {
+        try {
+            Files.write(Paths.get(PATH, "hello.txt"), "Hello, World!".getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (GitRepository repo = gitRepositoryOpen(PATH);
+             GitIndex index = gitRepositoryIndex(repo)) {
+            Index.gitIndexAddByPath(index, "hello.txt");
+            Index.gitIndexWrite(index);
+            assertDoesNotThrow(() -> Index.gitIndexWriteTree(index));
+        }
+    }
+}
