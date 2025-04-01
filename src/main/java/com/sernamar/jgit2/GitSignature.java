16c92d0d6ea13baeca4675265e643c8e377baec3
sernamar <sernamar@protonmail.com>
2025-04-01 17:37:00 +0200

    Remove comment in GitSignature class
    The comment mentions what is now discussed in issue #5.

--- src/main/java/com/sernamar/jgit2/GitSignature.java
+++ src/main/java/com/sernamar/jgit2/GitSignature.java
@@ -5,16 +5,7 @@ import com.sernamar.jgit2.bindings.git_signature;
 import java.lang.foreign.MemorySegment;
 
 import static com.sernamar.jgit2.bindings.git2_1.git_signature_free;
-/*
-`GitSignature` objects can be created in two ways:
-1. Using functions like `gitSignatureNew`, `gitSignatureNow`, or `gitSignatureDefault`, that create signatures that
- are owned by the caller.
-2. Using functions like `gitCommitAuthor`, `gitCommitCommiter`, `gitSignatureLookup`, etc., that return signatures that
- are not owned by the caller.
-The `owned` field indicates whether the `GitSignature` object is owned by the caller or not.
-Only `GitSignature` objects that are owned by the caller should be freed using `git_signature_free`, which is called in the
-`close` method.
- */
+
 public final class GitSignature implements AutoCloseable {
     private MemorySegment segment;
     private final boolean owned;
