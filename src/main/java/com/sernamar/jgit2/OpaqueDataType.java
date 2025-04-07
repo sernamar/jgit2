65c3ce7758ac6ff4ca19d39a5e2f856eba9a176d
sernamar <sernamar@protonmail.com>
2025-04-07 16:10:57 +0200

    Add class description for OpaqueDataType

--- src/main/java/com/sernamar/jgit2/OpaqueDataType.java
+++ src/main/java/com/sernamar/jgit2/OpaqueDataType.java
@@ -2,6 +2,13 @@ package com.sernamar.jgit2;
 
 import java.lang.foreign.MemorySegment;
 
+/*
+ * This class represents an opaque data type in the libgit2 library.
+ * Examples:
+ * - `typedef struct git_repository git_repository`
+ * - `typedef struct git_tree git_tree`
+ * - `typedef struct git_commit git_commit`
+ */
 abstract class OpaqueDataType implements AutoCloseable {
     private MemorySegment segment; // zero-length memory segment
     private final boolean owned;
