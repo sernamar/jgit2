810a8ad43f8a5abec4e1e2020b7dbb78e6e7c06e
sernamar <sernamar@protonmail.com>
2025-04-08 06:34:03 +0200

    Enhance OpaqueDataType documentation

--- src/main/java/com/sernamar/jgit2/OpaqueDataType.java
+++ src/main/java/com/sernamar/jgit2/OpaqueDataType.java
@@ -3,11 +3,21 @@ package com.sernamar.jgit2;
 import java.lang.foreign.MemorySegment;
 
 /*
- * This class represents an opaque data type in the libgit2 library.
- * Examples:
- * - `typedef struct git_repository git_repository`
- * - `typedef struct git_tree git_tree`
- * - `typedef struct git_commit git_commit`
+ * Abstract class representing an opaque data type.
+ *
+ * A zero-length memory segment is used to represent a pointer to an opaque data structure used or allocated by a
+ * foreign function. The segment consists of two values: the first is a valid pointer to the foreign memory region,
+ * and the second is the size of the region. Since the Java runtime cannot determine the size of the region, it is
+ * set to zero, which is why the segment is referred to as a "zero-length memory segment". This segment acts as a
+ * placeholder that can later be used to reference the actual memory of the opaque structure. The size of the region
+ * is implicitly determined by the native library, and the segment itself cannot be accessed directly. It can only be
+ * passed to other foreign functions that accept pointers to such opaque structures.
+ * See:
+ * https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/lang/foreign/MemorySegment.html#wrapping-addresses
+ *
+ * This class implements AutoCloseable to ensure that memory management is properly handled, automatically freeing the
+ * memory when the object is no longer needed. The close() method will invoke the free() method to release memory if
+ * the object owns it.
  */
 abstract class OpaqueDataType implements AutoCloseable {
     private MemorySegment segment; // zero-length memory segment
